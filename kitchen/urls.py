b940b911c638d8c3907c3cdd261ddd301484dc8a
deepakbartwal <deepak.bartwal@outlook.com>
2017-01-21 10:10:42 +0530

    worked on pdp

--- kitchen/urls.py
+++ kitchen/urls.py
@@ -3,7 +3,7 @@ from kitchen.views import *
 
 urlpatterns = [
     # url(r'^test/', Test, name='test'),
-    url(r'^kitchen-response', KitchenResponse, name='kitchen-response'),
+    # url(r'^kitchen-response', KitchenResponse, name='kitchen-response'),
     url(r'^(?P<k_type_slug>[-\w]+)/(?P<theme_slug>[-\w]+)/(?P<kitchen_slug>[-\w]+)/$', ServeProduct),
     url(r'^(?P<k_type_slug>[-\w]+)/(?P<theme_slug>[-\w]+)/$', ServeKitchen),
     url(r'^(?P<k_type_slug>[-\w]+)/$', ServeTheme),
