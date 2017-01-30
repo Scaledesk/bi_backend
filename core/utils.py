188b7f07d2ed924e19cabd0176bc9f6e3f635914
garima <garima8jun@gmail.com>
2017-01-30 13:20:51 +0530

    conflic resolve

--- core/utils.py
+++
@@ -1,8 +0,0 @@
-from core.models import *
-# from pprint import pprint
-
-def AppendBasicContext(context):
-    """ To create common context which will be used in all templates (mainly in header and footer) """
-    context['k_types'] = KType.objects.all()
-    context['w_types'] = WType.objects.all()
-    return context
