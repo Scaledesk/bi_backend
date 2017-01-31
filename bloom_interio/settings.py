e03af359bfde1985e2f1cf715e8246fe9d56206e
garima <garima8jun@gmail.com>
2017-01-31 11:30:23 +0530

    ui changes and content updation

--- bloom_interio/settings.py
+++ bloom_interio/settings.py
@@ -100,7 +100,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'bloom_interio',
         'USER': 'root',
-        'PASSWORD': 'as',
+        'PASSWORD': 'password',
         'HOST': '127.0.0.1',
         'PORT': '3306',
     }
