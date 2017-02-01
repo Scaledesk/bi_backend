7b402df834ef21cf8bbc8e7316c36a7380023245
deepakbartwal <deepak.bartwal@outlook.com>
2017-02-01 22:24:15 +0530

    changes in footer

--- bloom_interio/settings.py
+++ bloom_interio/settings.py
@@ -99,7 +99,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'bloom_interio',
         'USER': 'root',
-        'PASSWORD': 'password',
+        'PASSWORD': 'as',
         'HOST': '127.0.0.1',
         'PORT': '3306',
     }
