1302a3d722e067d56d001cda12d369add57936bc
garima <garima8jun@gmail.com>
2017-02-14 12:17:49 +0530

    ui changes

--- bloom_interio/settings.py
+++ bloom_interio/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '@wqsdsfv332gy*thc7rmn5b(4i*p+45aj$3&vm0@@%blbd$^20'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.1.4']
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
