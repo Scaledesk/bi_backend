9dc5d83f5eacbe16ce33ad10c25e9e248fb1059b
Gaurav <lakhani@scaledesk.com>
2017-10-25 14:25:54 +0530

    SEO Changes

--- core/utils.py
+++ core/utils.py
@@ -6,6 +6,6 @@ def AppendBasicContext(context):
     context['k_types'] = KType.objects.all()
     context['w_types'] = WType.objects.all()
     context['meta_title'] = 'Bloom Interio - Modular Kitchen Manufacturer Delhi, Gurgaon & Noida'
-    context['meta_desc'] = 'Bloom Interio provides a range of beautiful modular kitchens, wardrobes & customised furniture to adorn your home interiors. Check modular kitchen price and cost with our price calculator.'
-    context['meta_keyword'] = 'modular kitchen price, modular kitchen gurgaon, modular kitchen in gurgaon, modular kitchen manufacturer, modular kitchen cost, modular kitchen dealers, best modular kitchen, cost of modular kitchen, modular kitchen designer, budget modular kitchen'
+    context['meta_desc'] = 'Bloom Interio provides a range of beautiful modular kitchens, wardrobes & furniture to adorn your home. Check modular kitchen price with our price calculator'
+    context['meta_keyword'] = 'modular kitchen price, modular kitchen gurgaon, modular kitchen in gurgaon, modular kitchen manufacturer, modular kitchen cost, modular kitchen dealers, best modular kitchen, budget modular kitchen'
     return context
